255fa23286d2c501cf65868c037378c7d8572b10
cjycjy666 <2228791577@qq.com>
2024-06-05 16:13:06 +0800

    first commit

---
+++ student-inquire-backend/src/main/java/com/cjy/studentinquirebackend/StudentInquireBackendApplication.java
@@ -0,0 +1,14 @@
+package com.cjy.studentinquirebackend;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentInquireBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentInquireBackendApplication.class, args);
+    }
+
+}
